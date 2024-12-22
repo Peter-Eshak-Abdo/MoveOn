5f5e5f822bcd7d71f3391d348ff693ef125efb75
Peter-Eshak-Abdo <mixcolor11@outlook.com>
2024-12-22 18:55:22 +0200

    some edit

--- public/js/admin_trip_form.js
+++ public/js/admin_trip_form.js
@@ -1,33 +1,20 @@
-document.getElementById('trip-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    const departureDate = document.getElementById('departure-date').value;
-    const departureTime = document.getElementById('departure-time').value;
-    const fromLocation = document.getElementById('destination').value;
-    const toLocation = document.getElementById('location').value;
+document.getElementById("trip-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const departureDate = document.getElementById("departure-date").value;
+  const departureTime = document.getElementById("departure-time").value;
+  const fromLocation = document.getElementById("destination").value;
+  const toLocation = document.getElementById("location").value;
 
-   
-    if (!departureDate) {
-        alert('Please select a specific date.');
-        return;
-    }
+  if (!departureDate) alert("Please select a specific date.");
 
-    if (!departureTime) {
-        alert('Please select a specific time.');
-        return;
-    }
+  if (!departureTime) alert("Please select a specific time.");
 
-    if (fromLocation === toLocation) {
-        alert(' destinations cannot be the same.');
-        return;
-    }
+  if (fromLocation === toLocation) alert(" destinations cannot be the same.");
 
-    console.log(`Date: ${departureDate}`);
-    console.log(`Time: ${departureTime}`);
-    console.log(`From: ${fromLocation}`);
-    console.log(`To: ${toLocation}`);
+  console.log(`Date: ${departureDate}`);
+  console.log(`Time: ${departureTime}`);
+  console.log(`From: ${fromLocation}`);
+  console.log(`To: ${toLocation}`);
 
-    
-    alert('New Trip  added successfully!');
-
-  
+  alert("New Trip  added successfully!");
 });
--- public/js/validation.js
+++ public/js/validation.js
@@ -5,49 +5,49 @@ const nameRegex = /^[a-zA-Z]+$/;
 
 // دالة التحقق من تسجيل الدخول
 function validateLoginForm() {
-    const usernameOrEmail = document.querySelector('#login input[placeholder="Username or Email"]').value;
-    const password = document.querySelector('#login input[placeholder="Password"]').value;
+  const usernameOrEmail = document.querySelector('#login input[placeholder="Username or Email"]').value;
+  const password = document.querySelector('#login input[placeholder="Password"]').value;
 
-    if (!emailRegex.test(usernameOrEmail)) {
-        alert("يرجى إدخال بريد إلكتروني صالح.");
-        return false;
-    }
+  if (!emailRegex.test(usernameOrEmail)) {
+    alert("يرجى إدخال بريد إلكتروني صالح.");
+    return false;
+  }
 
-    if (!passwordRegex.test(password)) {
-        alert("كلمة المرور يجب أن تحتوي على حرف كبير، حرف صغير، ورقم، ويجب أن تكون أطول من 8 أحرف.");
-        return false;
-    }
+  if (!passwordRegex.test(password)) {
+    alert("كلمة المرور يجب أن تحتوي على حرف كبير، حرف صغير، ورقم، ويجب أن تكون أطول من 8 أحرف.");
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 // دالة التحقق من نموذج التسجيل
 function validateRegisterForm() {
-    const firstName = document.querySelector('#register input[placeholder="Firstname"]').value;
-    const lastName = document.querySelector('#register input[placeholder="Lastname"]').value;
-    const email = document.querySelector('#register input[placeholder="Email"]').value;
-    const password = document.querySelector('#register input[placeholder="Password"]').value;
+  const firstName = document.querySelector('#register input[placeholder="Firstname"]').value;
+  const lastName = document.querySelector('#register input[placeholder="Lastname"]').value;
+  const email = document.querySelector('#register input[placeholder="Email"]').value;
+  const password = document.querySelector('#register input[placeholder="Password"]').value;
 
-    if (!nameRegex.test(firstName)) {
-        alert("الاسم الأول يجب أن يحتوي على حروف فقط.");
-        return false;
-    }
+  if (!nameRegex.test(firstName)) {
+    alert("الاسم الأول يجب أن يحتوي على حروف فقط.");
+    return false;
+  }
 
-    if (!nameRegex.test(lastName)) {
-        alert("الاسم الأخير يجب أن يحتوي على حروف فقط.");
-        return false;
-    }
+  if (!nameRegex.test(lastName)) {
+    alert("الاسم الأخير يجب أن يحتوي على حروف فقط.");
+    return false;
+  }
 
-    if (!emailRegex.test(email)) {
-        alert("يرجى إدخال بريد إلكتروني صالح.");
-        return false;
-    }
+  if (!emailRegex.test(email)) {
+    alert("يرجى إدخال بريد إلكتروني صالح.");
+    return false;
+  }
 
-    if (!passwordRegex.test(password)) {
-        alert("كلمة المرور يجب أن تحتوي على حرف كبير، حرف صغير، ورقم، ويجب أن تكون أطول من 8 أحرف.");
-        return false;
-    }
+  if (!passwordRegex.test(password)) {
+    alert("كلمة المرور يجب أن تحتوي على حرف كبير، حرف صغير، ورقم، ويجب أن تكون أطول من 8 أحرف.");
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
